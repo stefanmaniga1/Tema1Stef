ea5ef5bc468d939e2dfbcd540af997251461cb89
BojteTamas <bojtetomi92@gmail.com>
2019-10-21 19:29:28 +0300

    Troll

--- Tema/src/Main.java
+++ Tema/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tema nr 1");
+        System.out.println("Tema tema tema cincnsdsksm");
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Tema tema tema cincnsdsksm");
     }
 }
